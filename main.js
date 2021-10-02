28c8c944000d6a30547049a7ec006267c0e8e586
SilverMoon23 <sashakucar200215@gmail.com>
2021-10-02 13:07:59 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let choice = confirm("Are you here?"); // возвращает true/false
+console.log('choice: ', choice);
+
+let age = prompt("Введите ваш возраст!", "18"); /* возвращает введенные данные в виде строки(string), 
+                                                   если данные не введены, вводит значение "18" */
+let ageNumber = +prompt("Введите ваш возраст!", "18"); /* "+" возвращает введенные данные в виде числа(number) */
+console.log('age: ', age);
+
+let answers = [];
+
+answers[0] = prompt("Введите ваше имя", "");
+answers[1] = prompt("Введите вашу фамилию", "");
+answers[2] = +prompt("Введите ваш возраст", "");
+
